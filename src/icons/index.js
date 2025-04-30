fab923ee3183b35c42e7319842a2ce5fe99d82e0
Matt Gehrls <matt@bootcampcentral.com>
2025-04-30 16:30:01 -0400

    added more icons

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,5 +1,29 @@
-import Hamburger from "./Hamburger";
+import Alert from "./Alert";
+import ChevronDown from "./ChevronDown";
 import Close from "./Close";
+import Employees from "./Employees";
+import Hamburger from "./Hamburger";
+import Products from "./Products";
+import Purchasing from "./Purchasing";
+import Sales from "./Sales";
+import Search from "./Search";
+import SeeMore from "./SeeMore";
+import Settings from "./Settings";
 import UserIcon from "./UserIcon";
 
-export {Hamburger, Close, UserIcon}
+//Alert was the last one done
+
+export {
+    Alert,
+    ChevronDown,
+    Close, 
+    Employees, 
+    Hamburger, 
+    Purchasing, 
+    Products, 
+    Sales, 
+    Search,
+    SeeMore,
+    Settings, 
+    UserIcon
+}
