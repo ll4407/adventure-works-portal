e04f7b83aabbaa3009d937b0d43a63f889e30d2f
Matt Gehrls <matt@bootcampcentral.com>
2025-05-01 14:57:54 -0400

    updates icons to be more versatile

--- src/icons/Minus.jsx
+++ src/icons/Minus.jsx
@@ -1,11 +1,12 @@
-export default function Minus({size, color}){
+export default function Minus({size, color, className}){
     return(
         <svg 
+            className={className ?? className}  
             xmlns="http://www.w3.org/2000/svg" 
             height={size ? `${size}px` :"24px"} 
             viewBox="0 -960 960 960" 
             width={size ? `${size}px` :"24px"} 
-            fill={color ?? "#e3e3e3"}>
+            fill={color ?? "currentColor"}>
                 <path d="M200-440v-80h560v80H200Z"/>
         </svg>
     )
