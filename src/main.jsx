cc4d586f4ddf13619ded89e2ed3f8133a1c9f943
Leuber Leuterio <leuber@bootcampcentral.com>
2025-05-02 15:13:45 -0400

    Removes AuthProvider import in main

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import accountReducer from './store/account';  
-// import { AuthProvider } from './context/AuthProvider.jsx'
+
 
 const store = configureStore({
   reducer: {
