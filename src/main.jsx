0cef3fc77ad272845338a3696b1edcaba71c91ca
Leuber Leuterio <leuber@bootcampcentral.com>
2025-05-02 15:00:57 -0400

    Removes <AuthProvider> in main

--- src/main.jsx
+++ src/main.jsx
@@ -19,9 +19,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        {/* <AuthProvider> */}
           <App />
-        {/* </AuthProvider> */}
       </BrowserRouter>
     </Provider>
   </StrictMode>,
