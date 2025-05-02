a05634acacda2b86834aa88c640036a856854d7d
Matt Gehrls <matt@bootcampcentral.com>
2025-05-02 16:53:52 -0400

    removing layouts, updated imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
-import Employee from './container/Employee/EmployeeSetup';
-import Product from './container/Product/ProductSetup';
-import Purchasing from './container/Purchasing/PurchasingSetup';
-import Sale from './container/Sale/SaleSetup';import Login from './components/Login/Login';
+import Employees from './container/Employee/Employees';
+import Products from './container/Product/Products';
+import Purchasing from './container/Purchasing/Purchasing';
+import Sales from './container/Sale/Sales';import Login from './components/Login/Login';
 
 function App() {
 
@@ -11,10 +11,10 @@ function App() {
     <Routes>
       <Route path='/' element={<Login/>} />
       <Route path='/dashboard' element={<Layout><h1>Dashboard</h1></Layout>} />
-      <Route path='/employees' element={<Layout><Employee /></Layout>} />
-      <Route path='/products' element={<Layout><Product /></Layout>} />
-      <Route path='/purchasing' element={<Layout><Purchasing /></Layout>} />
-      <Route path='/sales' element={<Layout><Sale /></Layout>} />
+      <Route path='/employees' element={<Employees />} />
+      <Route path='/products' element={<Products />} />
+      <Route path='/purchasing' element={<Purchasing />} />
+      <Route path='/sales' element={<Sales />} />
       <Route path='/settings' element={<Layout><h1>Settings</h1></Layout>} />
     </Routes>
   )
