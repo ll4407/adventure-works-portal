6d0381e5dfa222cb4cb5c92724c293f985d89f14
Matt Gehrls <matt@bootcampcentral.com>
2025-05-03 12:41:13 -0400

    remove logic from headerSearch

--- src/components/SectionHeader/HeaderSearch.jsx
+++ src/components/SectionHeader/HeaderSearch.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useContext, useCallback } from 'react';
+import { useContext } from 'react';
 import styles from './SectionHeader.module.css';
 import { Search } from '../../icons';
 import PageContext from '../../context/PageContext';
@@ -8,39 +8,19 @@ function HeaderSearch(){
 
     const {filter, setFilter} = useContext(PageContext)
 
-    const handleSearch = useCallback(() => {
-        if(filter){
-            alert('new filter = ', filter)
-        }
-    }, [filter])
-
-    useEffect(() =>{
-
-        const timer = setTimeout(()=>{
-
-            handleSearch()
-
-        }, 500)
-
-        return () => clearTimeout(timer)
-
-    },[filter, handleSearch])
-
     return(
         <div className={styles.headerSearch}>
-            <form onSubmit={(evt) => {
-                evt.preventDefault()
-                handleSearch()
-            }}>
-                <div>
-                    <label htmlFor='search'>
-                        <input type="text" id='search' value={filter} onChange={evt => setFilter(evt.target.value)} />
-                    </label>
-                    <button aria-label='search' type='submit'>
-                        <Search size={24} />
-                    </button>
-                </div>
-            </form>
+            <label className={styles.searchLabel} htmlFor='search'>
+                <input 
+                    type="text" 
+                    id='search'
+                    className={styles.searchInput}
+                    value={filter} 
+                    onChange={evt => setFilter(evt.target.value)} />
+            </label>
+            <div className={styles.searchBtn}>
+                <Search size={24} />
+            </div>
         </div>
     )}
 
