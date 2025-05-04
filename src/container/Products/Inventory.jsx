a68c770ff7b8f66370711206e8f0832c424bd11b
Matt Gehrls <matt@bootcampcentral.com>
2025-05-04 14:57:56 -0400

    grid styling for inventory complete

--- src/container/Products/Inventory.jsx
+++ src/container/Products/Inventory.jsx
@@ -1,4 +1,5 @@
-import { ChevronDown } from '../../icons'
+import clsx from 'clsx'
+import { ChevronDown, Delete, Edit } from '../../icons'
 import ProductModal from './ProductModal'
 import styles from './Products.module.css'
 import { useState } from 'react'
@@ -24,13 +25,44 @@ const Inventory = (props) =>{
     return(
         <>
         <div className={styles.productList}>
+            <div className={clsx(styles.productCard, styles.tableHeader)}>
+                <p className={clsx(styles.column, styles.bold, styles.col1)}>
+                    Product Name
+                </p>
+                <p className={clsx(styles.column, styles.bold, styles.col2)}>
+                    Qty
+                </p>
+                <p className={clsx(styles.column, styles.bold, styles.col3)}>
+                    Product ID
+                </p>
+                <p className={clsx(styles.column, styles.bold, styles.col4)}>
+                    Location
+                </p>
+                <p className={clsx(styles.column, styles.bold, styles.col5)}>
+                    Shelf
+                </p>
+                <p className={clsx(styles.column, styles.bold, styles.col6)}>
+                    Bin
+                </p>
+                <p className={clsx(styles.column, styles.bold, styles.col7)}>
+                    Options
+                </p>
+            </div>
             {products.map(prod => (
-                <button className={styles.productCard} onClick={() => setActiveProduct(prod)}>
-                    <div className={styles.column}>
+                <button key={prod.productId + prod.productName + prod.locationId + prod.locationName} className={styles.productCard} onClick={() => setActiveProduct(prod)}>
+                    <div className={clsx(styles.column, styles.col1)}>
                         <p className={styles.productName}>{prod.productName}</p>
-                        <p>{prod.locationName}</p>
+                        <p className={styles.extraLocation}>{prod.locationName}</p>
                     </div>
-                    <p>QTY {prod.quantity}</p>
+                    <p className={clsx(styles.column, styles.col2)}><span className={styles.quantitySpan}>QTY </span>{prod.quantity}</p>
+                    <p className={clsx(styles.column, styles.col3)}>{prod.productId}</p>
+                    <p className={clsx(styles.column, styles.col4)}>{prod.locationName}</p>
+                    <p className={clsx(styles.column, styles.col5)}>{prod.shelf}</p>
+                    <p className={clsx(styles.column, styles.col6)}>{prod.bin}</p>
+                    <div className={clsx(styles.col7)}>
+                        <Edit/>
+                        <Delete />
+                    </div>
                     <ChevronDown size={30} className={styles.chevron} />
                 </button>
             ))}
