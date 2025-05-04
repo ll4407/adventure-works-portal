2ccf388479274c58c3b4e3f8683e1b8606f5d00a
Jacob Heiser <jacob@bootcampcentral.com>
2025-05-04 17:06:12 -0400

    Purchase Vendor: Responsive layout

--- src/components/Purchasing/PurchasingVendorTile.jsx
+++ src/components/Purchasing/PurchasingVendorTile.jsx
@@ -1,22 +1,26 @@
-import { Edit, Delete } from '../../icons';
+import { Edit, Delete, ChevronDown } from '../../icons';
 import styles from '../../container/Purchasing/Purchasing.module.css';
 import { Link } from 'react-router-dom';
 
 const PurchasingVendorTile = props => {
     return (
         <section className={styles.VendorGridContent}>
-            <p>{props.vendorName}</p>
-            <p>{props.phone}</p>
-            <p>{props.businessId}</p>
-            <p>{props.primaryContact}</p>
-            <p>{props.email}</p>
             <div>
-                <p>{props.addressLine}</p>
-                <p>{props.addressLine2}</p>
-                <p>{props.city}, {props.state}</p>
-                <p>{props.postal}</p>
+                <p>{props.vendorName}</p>
+                <p>{props.phone}</p>
+                <p>{props.businessId}</p>
+                <p>{props.primaryContact}</p>
+                <p>{props.email}</p>
+                <div>
+                    <p>{props.addressLine}</p>
+                    <p>{props.addressLine2}</p>
+                    <p>{props.city}, {props.state}</p>
+                    <p>{props.postal}</p>
+                </div>
+                <p><Link aria-label='Edit Button' to={'/purchasing/' + props.businessId}><Edit /></Link> <Delete /></p>
             </div>
-            <p><Link to={'/purchasing/' + props.businessId}><Edit /></Link> <Delete /></p>
+
+            <div><Link aria-label='View More Button' to={'/purchasing/' + props.businessId}><ChevronDown size={36} /></Link></div>
         </section>
     )
 }
