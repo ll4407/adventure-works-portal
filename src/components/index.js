00aca1d3dc35f1d466a19a1637836ac6b054e559
Matt Gehrls <matt@bootcampcentral.com>
2025-05-17 12:07:43 -0400

    employee basic set up

--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,11 @@ import Layout from "./Layout/Layout";
 import Login from "./Login/Login";
 import ProtectedRoute from "./Login/ProtectedRoute";
 import SectionHeader from "./SectionHeader/SectionHeader";
+import Overview from "./Employees/Overview";
 
 export * from "./Products"
 export {
+  Overview,
   Layout,
   Login,
   ProtectedRoute,
--- src/container/Employees/Employees.jsx
+++ src/container/Employees/Employees.jsx
@@ -1,14 +1,30 @@
+import { Outlet } from "react-router";
+import PageContext from "../../context/PageContext";
+import usePageContext from "../../hooks/usePageContext";
+
 import SectionHeader from "../../components/SectionHeader/SectionHeader";
 
-function Employees(){
-    return(
-        <>
-        <SectionHeader
-            title={"Employees"}
-            color={"orange"}
-            firstButton={"Overview"} />
-        </>
-    )
-}
+import styles from './Employees.module.css'
+import { Overview } from "../../components";
 
-export default Employees;
+export default function Employees(){
+    const pageContext = usePageContext({
+        firstBtnUrl: '/employees', 
+    })
+
+
+    return(
+        <PageContext.Provider value={pageContext}>
+            <SectionHeader
+                title={"Employees"}
+                color={"orange"}
+                firstButton={'Overview'}
+                />
+            <div className={styles.contentWrapper}>
+                <Overview />
+            </div>
+        </PageContext.Provider>
+
+    )
+
+}
