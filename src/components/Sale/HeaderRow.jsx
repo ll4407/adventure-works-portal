6e0c3c50cee949f2a07deaed9ac4946cea82a98a
Matt Gehrls <matt@bootcampcentral.com>
2025-05-27 09:57:15 -0400

    sales accessibility

--- src/components/Sale/HeaderRow.jsx
+++ src/components/Sale/HeaderRow.jsx
@@ -4,24 +4,24 @@ const HeaderRow = ({ isCustomer, handleSort }) => (
     <div className={styles.headerRow}>
       {isCustomer ? (
         <>
-          <span onClick={() => {handleSort("lastName", "name"); }}>Customer&#x25BE;</span>
-          <span onClick={() => {handleSort("orderDate", "date"); }}>Order Date&#x25BE;</span>
-          <span onClick={() => {handleSort("orderNumber", ""); }}>Order Number&#x25BE;</span>
-          <span onClick={() => {handleSort("orderQty", ""); }}>Order Qty&#x25BE;</span>
-          <span onClick={() => {handleSort("shipDate", "date"); }}>Ship Date&#x25BE;</span>
-          <span onClick={() => {handleSort("unitPrice", ""); }}>Unit Price&#x25BE;</span>
-          <span onClick={() => {handleSort("lineTotal", ""); }}>Total Due&#x25BE;</span>
+          <button aria-label="Sort by customer last name" onClick={() => {handleSort("lastName", "name"); }}>Customer&#x25BE;</button>
+          <button aria-label="Sort by order date" onClick={() => {handleSort("orderDate", "date"); }}>Order Date&#x25BE;</button>
+          <button aria-label="Sort by order number" onClick={() => {handleSort("orderNumber", ""); }}>Order Number&#x25BE;</button>
+          <button aria-label="Sort by order quantity" onClick={() => {handleSort("orderQty", ""); }}>Order Qty&#x25BE;</button>
+          <button aria-label="Sort by ship date" onClick={() => {handleSort("shipDate", "date"); }}>Ship Date&#x25BE;</button>
+          <button aria-label="Sort by unit price" onClick={() => {handleSort("unitPrice", ""); }}>Unit Price&#x25BE;</button>
+          <button aria-label="Sort by total due" onClick={() => {handleSort("lineTotal", ""); }}>Total Due&#x25BE;</button>
           <span />
         </>
       ) : (
         <>
-          <span onClick={() => {handleSort("storeName", "name"); }}>Store/Business&#x25BE;</span>
-          <span onClick={() => {handleSort("orderDate", "date"); }}>Order Date&#x25BE;</span>
-          <span onClick={() => {handleSort("contactLastName", "name"); }}>Contact Name&#x25BE;</span>
-          <span onClick={() => {handleSort("orderNumber", ""); }}>Order Number&#x25BE;</span>
-          <span onClick={() => {handleSort("productName", "name"); }}>Product Name&#x25BE;</span>
-          <span onClick={() => {handleSort("unitPrice", ""); }}>Unit Price&#x25BE;</span>
-          <span onClick={() => {handleSort("lineTotal", ""); }}>Total Due&#x25BE;</span>
+          <button aria-label="Sort by store/business" onClick={() => {handleSort("storeName", "name"); }}>Store/Business&#x25BE;</button>
+          <button aria-label="Sort by order date" onClick={() => {handleSort("orderDate", "date"); }}>Order Date&#x25BE;</button>
+          <button aria-label="Sort by contact name" onClick={() => {handleSort("contactLastName", "name"); }}>Contact Name&#x25BE;</button>
+          <button aria-label="Sort by order number" onClick={() => {handleSort("orderNumber", ""); }}>Order Number&#x25BE;</button>
+          <button aria-label="Sort by product number" onClick={() => {handleSort("productName", "name"); }}>Product Name&#x25BE;</button>
+          <button aria-label="Sort by unit price" onClick={() => {handleSort("unitPrice", ""); }}>Unit Price&#x25BE;</button>
+          <button aria-label="Sort by total due" onClick={() => {handleSort("lineTotal", ""); }}>Total Due&#x25BE;</button>
           <span />
         </>
       )}
